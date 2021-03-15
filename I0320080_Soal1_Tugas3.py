c18f01181b57f074f0c5ead7571a19fa33732870
rafliI0320080 <raflirdwn@student.uns.ac.id>
2021-03-15 19:27:04 +0700

    Tugas Prakprokom3

---
+++ I0320080_Soal1_Tugas3.py
@@ -0,0 +1,14 @@
+# first step
+my_list = ['Hana','Auwdri','Vian','Rian','Ojat','Alam','Hani','Vizal','Safri','Ivan']
+print("my_list[4]: ", my_list[4])
+print("my_list[6]: ", my_list[6])
+print("my_list[7]: ", my_list[7])
+print(my_list)
+# next step
+my_list[3] = 'sasa cantik'
+my_list[5] = 'micin sayang'
+my_list[9] = 'tunjung'
+j = 0
+for i in range (1,11):
+    print(my_list[j])
+    j+=1
---
+++ I0320080_soal2_Tugas3.py
@@ -0,0 +1,12 @@
+# dict
+Identitas = {"nama":"Rafli Ridwan R",
+            "Hobi 1":"Lari","Hobi 2":"Masak","Hobi 3":"Berkuda",
+            "Instagram":"eyanggledek","Facebook":"doublesaa","twitter":"panutanmu",
+            "Makanan 1":"timlo","Makanan 2":"sate","makanan 3":"rawon"}
+del Identitas['Makanan 1']
+del Identitas['Makanan 2']
+print(Identitas)
+Identitas['Hobi 1'] = 'Tidur'
+Identitas['Instagram'] = 'raflirdwn'
+Identitas['Hobi 4'] = 'Manah'
+print(Identitas)
